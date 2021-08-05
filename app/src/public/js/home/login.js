56bcca18434ddc7f1e9f6c5a0211d30b3ef486b6
yunwhanlee <71131615+yunwhanlee@users.noreply.github.com>
2021-08-05 15:26:38 +0900

    javascript Dom Event Login Btn

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict"
-
 console.log("HAHA HOHO!")
+
+const id = document.querySelector("#id")
+const pw = document.querySelector("#pw")
+const loginBtn = document.querySelector("button")
+
+loginBtn.addEventListener("click", (ev)=>login())
+
+function login(){
+    const req = {id: id.value, pw: pw.value}
+    console.log(req)
+}
