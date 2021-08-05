ff0a2f70da488504569853bb7f70e4fcd7ed4cad
yunwhanlee <71131615+yunwhanlee@users.noreply.github.com>
2021-08-05 14:57:22 +0900

    フロントエンドJS

--- app/app.js
+++ app/app.js
@@ -8,9 +8,9 @@ const home = require("./src/routes/home")
 //アプリセット
 app.set("views", "./src/views") //経路
 app.set("view engine", "ejs") //拡張子
+app.use(express.static(`${__dirname}/src/public`)) //__dirname : app.jsのパス
 
 //ミドルウェア登録(fromルーティング)
 app.use("/", home)
 
 module.exports = app
-
