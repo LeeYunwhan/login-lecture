11d3a5fa8bb95f883d192bfb683e1f38e358d54d
yunwhanlee <71131615+yunwhanlee@users.noreply.github.com>
2021-08-05 16:09:56 +0900

    ログインを要請するfetch()構造生成

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,8 +3,9 @@ const express = require("express")
 const router = express.Router()
 const ctrl  = require("./home.ctrl")
 
-//ejsファイルを開く(.ejsは省略可)
-router.get("/", ctrl.home)
-router.get("/login", ctrl.login)
+//ejsファイルを開く(.ejsは省略可)   
+router.get("/", ctrl.home) //照会
+router.get("/login", ctrl.login) 
+
 
 module.exports = router//外部ファイルとして露出
